919ea5c3ad11b2d6185a34b92b320cb1e78c29b4
João Henrique <joaohenriquetdj@gmail.com>
2021-12-03 11:58:51 -0300

    Add styling modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 
+import closeImg from '../../assets/close.svg'
 import Modal from 'react-modal'
+import { Container } from './style'
 
 interface NewTransactionModalProps {
   isOpen: boolean
@@ -8,8 +10,26 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onClose }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} >
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button
+        type="button"
+        onClick={onClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
