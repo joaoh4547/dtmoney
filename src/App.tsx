2d170f7c6defe937cc71f0fdf992270055a26d85
João Henrique <joaohenriquetdj@gmail.com>
2021-12-04 16:01:20 -0300

    Add transactions in context

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "./styles/global"
 import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement("#root")
 
@@ -19,7 +19,7 @@ export function App() {
         setIsNewTransactionModalOpen(false)
     }
     return (
-        <TransactionsContext.Provider value={[]}>
+        <TransactionsProvider>
             <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
             <NewTransactionModal
@@ -27,8 +27,7 @@ export function App() {
                 onClose={handleCloseNewTransactionModal}
             />
             <GlobalStyle />
-
-        </TransactionsContext.Provider>
+        </TransactionsProvider>
     );
 }
 
